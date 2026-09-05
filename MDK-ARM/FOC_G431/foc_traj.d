55c4ad9c35269cec28d14f940c57f3ffd565d222
Kyro-Qu <qushichao75@gmail.com>
2026-09-06 01:10:00 +0800

    build: add .gitignore for Keil MDK artifacts and Python logs
    Exclude intermediate build objects (*.o, *.crf, *.d, *.axf, *.hex,
    *.map, *.htm, *.dep), Keil workspace user files, and Python test
    artifacts. Keep FOC_G431.sct linker script under version control.

--- MDK-ARM/FOC_G431/foc_traj.d
+++
@@ -1,5 +0,0 @@
-foc_g431\foc_traj.o: Code\foc\Core\foc_traj.c
-foc_g431\foc_traj.o: Code\foc\Core\foc_traj.h
-foc_g431\foc_traj.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-foc_g431\foc_traj.o: Code\foc\Core\foc_utils.h
-foc_g431\foc_traj.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
--- MDK-ARM/FOC_G431/startup_stm32g431xx.d
+++
@@ -1,1 +0,0 @@
-foc_g431\startup_stm32g431xx.o: startup_stm32g431xx.s
